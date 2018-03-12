3ff8c5a83b35ccfc8755ac3fc4b3706eaf6d917b
richoandy <richoandy@gmail.com>
2018-03-12 16:08:30 +0700

    release 1

--- parser.js
+++ parser.js
@@ -24,15 +24,70 @@ class PersonParser {
 
     for (var i = 1; i < rows.length; i++) {
       var temp = rows[i].split(",")
-      var obj = new Person(temp[0], temp[1], temp[2], temp[3], temp[4], temp[5])
+      //2012-05-10T03:53:40-07:00
+      //new Date(year, month, day)
+      // var rawDate = temp[5].toString()
+      // var year =
+      // var month =
+      // var day =
+      // var d = new Date(year, month, day)
+      var obj = new Person(temp[0], temp[1], temp[2], temp[3], temp[4],  temp[5])
       this._people.push(obj)
     }
+
   }
 
-  getPeople() {
+  get file(){
+    return this._file
+  }
+
+  set file(file){
+    this._file = file
+  }
+
+  get people(){
     return this._people
+  }
+
+  set people(people){
+    this._people = people
+  }
+
+
+  get people() {
+    return this._people
+  }
+
+  getPeopleatIndex(index){
+    return this._people[index]
+  }
+
+  addPerson(id, firstName, lastName, email, phone, created_at) {
+    var tmpPerson = new Person(id, firstName, lastName, email, phone, created_at)
+    this._people.unshift(tmpPerson)
+  }
+
+  save(){
+    var fs = require("fs");
+    var str = ""
+
+    for (var i = 0; i < this._people.length; i++) {
+      if (this._people[i].id) {
+        str += this._people[i].id + "," +
+        this._people[i].firstName + "," +
+        this._people[i].lastName + "," +
+        this._people[i].email + "," +
+        this._people[i].phone + "," +
+        this._people[i].createdAt + '\n'
+      }
+    }
+    fs.writeFileSync(this._file, str, "utf8")
   }
 }
 
 let parser = new PersonParser('people.csv')
-console.log(parser.getPeople());
+
+//tambah orang
+parser.addPerson(188, "abc", "def", "abc@def", "12345", "today")
+parser.addPerson(1112, "abc", "def", "abc@def", "12345", "today")
+parser.save()
