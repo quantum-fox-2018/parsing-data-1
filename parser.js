405f98c19bf9682878d04784eb0ead927237d954
richoandy <richoandy@gmail.com>
2018-03-12 12:52:10 +0700

    done

--- parser.js
+++ parser.js
@@ -1,25 +1,38 @@
 "use strict"
 
+var str = ""
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+  constructor(id, firstName, lastName, email, phone, createdAt){
+    this.id = id
+    this.firstName = firstName
+    this.lastName = lastName
+    this.email = email
+    this.phone = phone
+    this.createdAt = createdAt
+  }
 }
 
 class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = []
+
+    var fs = require("fs");
+    var buf = fs.readFileSync(this._file, 'utf8');
+    var rows = buf.split("\n")
+
+    for (var i = 1; i < rows.length; i++) {
+      var temp = rows[i].split(",")
+      var obj = new Person(temp[0], temp[1], temp[2], temp[3], temp[4], temp[5])
+      this._people.push(obj)
+    }
   }
 
-  get people() {
+  getPeople() {
     return this._people
   }
-
-  addPerson() {}
-
 }
 
 let parser = new PersonParser('people.csv')
-
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+console.log(parser.getPeople());
